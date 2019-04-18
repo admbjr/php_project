ef2b09b6e1329de1d834ecd8aa023ce5a132a700
admbjr <36055206+admbjr@users.noreply.github.com>
2019-04-18 14:57:10 -0400

    .

--- GhostCoders/SQL DB FLIES/test_nomadic_escape.sql
+++ GhostCoders/SQL DB FLIES/test_nomadic_escape.sql
@@ -1,25 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 18, 2019 at 08:46 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Host: localhost:8889
+-- Generation Time: Apr 18, 2019 at 06:56 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `test_nomadic_escape`
+-- Database: `TEST_Nomadic_Escape`
 --
 
 -- --------------------------------------------------------
@@ -125,6 +117,20 @@ INSERT INTO `blog_comment` (`comment_id`, `user_id`, `blog_id`, `comment`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact_form`
+--
+
+CREATE TABLE `contact_form` (
+  `contact_id` int(11) NOT NULL,
+  `contact_name` varchar(200) NOT NULL,
+  `contact_email` varchar(200) NOT NULL,
+  `contact_subject` varchar(200) NOT NULL,
+  `contact_message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `destinations`
 --
 
@@ -144,6 +150,32 @@ INSERT INTO `destinations` (`des_id`, `image`, `caption`, `country`, `city`) VAL
 (13, 'pic03.jpg', 'Home of maple syrup', 'Canada', 'Toronto'),
 (15, 'headline3.jpeg', 'Incredible India', 'India', 'Delhi'),
 (16, 'pic06.jpg', 'Beautiful Island', 'Mauritius', 'Port Louis');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq`
+--
+
+CREATE TABLE `faq` (
+  `faq_id` int(11) NOT NULL,
+  `questions` text NOT NULL,
+  `answers` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `faq`
+--
+
+INSERT INTO `faq` (`faq_id`, `questions`, `answers`) VALUES
+(1, 'Is account registration required?', 'Account registration at Nomadic Escape is required for users to access website.\r\n'),
+(2, 'Can I submit my own pictures to this website?\r\n', 'YES! All of the content in the gallery has been submitted by the community. You are encouraged to contribute. All credits are published along with the resources.\r\n'),
+(3, 'What are the fees for using this website?\r\n', 'Everything is free!\r\n'),
+(4, 'Can post a link to my business page for free?\r\n', 'If you want to promote your company on our website, please send us an email and we will send you our rates. info@nomadicescape.com\r\n'),
+(5, 'Can anyone join this website?\r\n', 'YES! Every and anyone can join Nomadic Escape and share all content.\r\n'),
+(6, 'adadadad', 'adadada'),
+(8, 'asdadasd', 'adadadadada'),
+(9, 'adadada', 'adadadd');
 
 -- --------------------------------------------------------
 
@@ -171,30 +203,43 @@ INSERT INTO `gallery` (`img_id`, `image`, `destination`, `caption`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `hotels`
+--
+
+CREATE TABLE `hotels` (
+  `hotel_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `area` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotels`
+--
+
+INSERT INTO `hotels` (`hotel_id`, `name`, `description`, `address`, `area`) VALUES
+(9, 'Hilton', 'free breakfast and wifi', '123 airport rd', 'ajax'),
+(10, 'Marriott', 'Free wifi free parking', '123 humber drive', 'toronto'),
+(11, 'Monte Carlo', 'Free parking free wifi free breakfast', '123 memory lane', 'brampton');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `messages`
 --
 
 CREATE TABLE `messages` (
   `msg_id` int(11) NOT NULL,
-  `user_to` varchar(50) NOT NULL,
-  `user_from` varchar(50) NOT NULL,
+  `sender` varchar(60) NOT NULL,
+  `receiver` varchar(60) NOT NULL,
+  `subject` text NOT NULL,
   `body` text NOT NULL,
-  `date` datetime NOT NULL,
-  `opened` varchar(3) NOT NULL,
-  `viewed` varchar(3) NOT NULL,
-  `deleted` varchar(3) NOT NULL,
+  `reply` text NOT NULL,
+  `status` text NOT NULL,
+  `msg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`msg_id`, `user_to`, `user_from`, `body`, `date`, `opened`, `viewed`, `deleted`, `user_id`) VALUES
-(1, 'allan', 'jason', 'hey how are you', '2019-02-28 05:13:13', 'yes', 'yes', 'no', NULL),
-(2, 'jason', 'allan', 'i am ok how are you', '2019-02-28 07:17:10', 'yes', 'no', 'no', NULL),
-(3, 'jenny', 'adam', 'im hungry', '2019-02-28 07:17:10', 'no', 'no', 'no', NULL),
-(4, 'jessoca', 'jeff', 'im bored', '2019-02-13 12:25:20', 'yes', 'yes', 'no', NULL);
 
 -- --------------------------------------------------------
 
@@ -233,6 +278,35 @@ CREATE TABLE `posts` (
   `post_body` text NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `restaurant_id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `area` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`restaurant_id`, `name`, `description`, `address`, `area`) VALUES
+(1, 'Boston Pizza', 'Pizza sports bar with an extensive menu.\r\nOpen for lunch and dinner', '2458 QUEEN STREET EAST  BRAMPTON, ON', ''),
+(2, 'Shoeless Joe\'s Sports Grill', 'Late-night food · Outdoor seating · Great cocktails', '50 Biscayne Crescent, Brampton, ON', 'wwwww'),
+(3, 'Kelseys Original Roadhouse', 'Large portions of American fare & specialty cocktails in a casual setting with comfy booths & TVs.\r\nLate-night food · Outdoor seating · Great cocktails', '2870 Queen St E, Brampton, ON', ''),
+(4, 'Turtle Jack\'s Airport', 'Stylish local chain eatery serving a menu of Canadian fare & global eats in a relaxed setting.\r\nLate-night food · Outdoor seating · Great cocktails', '20 Cottrelle Blvd, Brampton, ON', ''),
+(15, 'My Billing Account', 'wwwww', 'wwww', 'wwwww'),
+(27, 'N00318388_a1', 'good food', '100 Speedwell St', 'pickering'),
+(28, 'test', 'gggg', '100 Speedwell St', 'brampton'),
+(29, '', '', '', ''),
+(30, '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -293,42 +367,15 @@ INSERT INTO `subscriber_news_letter` (`subscriber_id`, `subscriber_name`, `subsc
 -- --------------------------------------------------------
 
 --
--- Table structure for table `survey`
+-- Table structure for table `SURVEY`
 --
 
-CREATE TABLE `survey` (
+CREATE TABLE `SURVEY` (
   `s_question_id` int(11) NOT NULL,
   `s_question` varchar(500) NOT NULL,
   `s_feedback` varchar(1000) NOT NULL,
   `s_username` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `profile_picture` varchar(50) NOT NULL,
-  `role` varchar(20) NOT NULL,
-  `sign_up_date` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password`, `profile_picture`, `role`, `sign_up_date`) VALUES
-(11, 'Manpreet', 'Kaur', 'sidhum317@gmail.com', '17de6b857063759d22198c58ba0c7c25', 'pic03.jpg', 'Admin', '2019-03-31'),
-(20, 'Amandeep', 'Kaur', 'aman@gmail.com', '\r\n7fcefd57e1b4dd0aa950faffd1ef99a9', 'asideimage1.jpg', 'User', '2019-04-16'),
-(21, 'Admin', 'Admin', 'admin@gmail.com', 'e3afed0047b08059d0fada10f400c1e5', 'articleimage1.jpg', 'Admin', '2019-04-18'),
-(22, 'Reina', 'Sukraj', 'reina.sukraj@gmail.com', '202cb962ac59075b964b07152d234b70', 'pic02.jpg', 'User', '2019-04-18');
 
 --
 -- Indexes for dumped tables
@@ -354,16 +401,34 @@ ALTER TABLE `blog_comment`
   ADD PRIMARY KEY (`comment_id`);
 
 --
+-- Indexes for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  ADD PRIMARY KEY (`contact_id`);
+
+--
 -- Indexes for table `destinations`
 --
 ALTER TABLE `destinations`
   ADD PRIMARY KEY (`des_id`);
 
 --
+-- Indexes for table `faq`
+--
+ALTER TABLE `faq`
+  ADD PRIMARY KEY (`faq_id`);
+
+--
 -- Indexes for table `gallery`
 --
 ALTER TABLE `gallery`
   ADD PRIMARY KEY (`img_id`);
+
+--
+-- Indexes for table `hotels`
+--
+ALTER TABLE `hotels`
+  ADD PRIMARY KEY (`hotel_id`);
 
 --
 -- Indexes for table `messages`
@@ -386,6 +451,12 @@ ALTER TABLE `posts`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`restaurant_id`);
+
+--
 -- Indexes for table `reviews`
 --
 ALTER TABLE `reviews`
@@ -399,16 +470,10 @@ ALTER TABLE `subscriber_news_letter`
   ADD PRIMARY KEY (`subscriber_id`);
 
 --
--- Indexes for table `survey`
+-- Indexes for table `SURVEY`
 --
-ALTER TABLE `survey`
+ALTER TABLE `SURVEY`
   ADD PRIMARY KEY (`s_question_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -439,10 +504,22 @@ ALTER TABLE `destinations`
   MODIFY `des_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
+-- AUTO_INCREMENT for table `faq`
+--
+ALTER TABLE `faq`
+  MODIFY `faq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
   MODIFY `img_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `hotels`
+--
+ALTER TABLE `hotels`
+  MODIFY `hotel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -457,6 +534,12 @@ ALTER TABLE `news_letter`
   MODIFY `news_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
@@ -469,40 +552,11 @@ ALTER TABLE `subscriber_news_letter`
   MODIFY `subscriber_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `blog`
---
-ALTER TABLE `blog`
-  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Constraints for table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Constraints for table `reviews`
---
-ALTER TABLE `reviews`
-  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`user_id`);

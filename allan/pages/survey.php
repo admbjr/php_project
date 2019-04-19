9e1262e7023f57bc27793398524bd224a14bd507
Reina Sukraj <reina.sukraj@gmail.com>
2019-04-19 12:08:56 -0400

    faq files
    faq files

---
+++ allan/pages/survey.php
@@ -0,0 +1,27 @@
+<?php
+    require_once("../includes/header.php");
+    require_once("../includes/navbar.php");
+?>
+
+
+    <header class="masthead" style="background-image:url('../assets/img/survey.jpeg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="site-heading">
+                        <h1>Survey</h1><span class="subheading">Please give us your feedback!</span></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div>
+        <?php // place your file path to your ("foldername/file.php" file
+           // require_once("");
+        ?>
+    </div>
+    
+
+<?php
+    require_once("../includes/footer.php");
+?>
